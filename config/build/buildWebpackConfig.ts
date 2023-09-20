df1a0863d4fa67a44b121b3cde00560ff1e37c9d
David Shabelian <dshabelyan@gmail.com>
2023-09-20 09:48:32 +0300

    feat: setting routes

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -4,7 +4,7 @@ import path from "path";
 import {buildPlugins} from "./buildPlugins";
 import {buildLoaders} from "./buildLoaders";
 import {buildResolvers} from "./buildResolvers";
-import { buildDevServer } from "./buildDevServer";
+import {buildDevServer} from "./buildDevServer";
 
 export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
     const {paths, mode, isDev} = options;
@@ -19,7 +19,7 @@ export function buildWebpackConfig(options: BuildOptions): webpack.Configuration
         },
         plugins: buildPlugins(options),
         module: {
-            rules: buildLoaders(),
+            rules: buildLoaders(options),
         },
         resolve: buildResolvers(),
         devtool: isDev ? 'inline-source-map' : undefined,
---
+++ src/components/Container/Container.tsx
@@ -0,0 +1,9 @@
+const Container = () => {
+    return (
+        <div>
+            <button>qwe</button>
+        </div>
+    );
+};
+
+export default Container;
